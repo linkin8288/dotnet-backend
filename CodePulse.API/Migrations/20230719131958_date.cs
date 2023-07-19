69a801afe079ab22ea82df9cf847891559c3f7e6
charlie <linkin8288@gmail.com>
2023-07-19 16:11:44 +0200

    Markdown and Image Preview

---
+++ CodePulse.API/Migrations/20230719131958_date.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodePulse.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class date : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublishsedDate",
+                table: "BlogPosts",
+                newName: "PublishedDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublishedDate",
+                table: "BlogPosts",
+                newName: "PublishsedDate");
+        }
+    }
+}
--- CodePulse.API/Models/Domain/BlogPost.cs
+++ CodePulse.API/Models/Domain/BlogPost.cs
@@ -8,10 +8,8 @@
         public string Content { get; set; }
         public string FeaturedImageUrl { get; set; }
         public string UrlHandle { get; set; }
-        public DateTime PublishsedDate { get; set; }
-
+        public DateTime PublishedDate { get; set; }
         public string Author { get; set; }
         public bool IsVisible { get; set; }
-        public DateTime PublishedDate { get; internal set; }
     }
 }
